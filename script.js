01885a7535a6e6744cc397d3511d4c9e375a8f18
uvsingh01 <104893892+uvsingh01@users.noreply.github.com>
2022-08-08 15:53:58 +0530

    second last

--- script.js
+++ script.js
@@ -2,39 +2,37 @@ const currencyOne= document.getElementById('currency-first');
 const currencyTwo= document.getElementById('currency-second');
 const amountOne = document.getElementById('amount-first');
 const amountTwo = document.getElementById('amount-second');
+const currencyOneValue = currencyOne.value;
+const currencyTwoValue = currencyTwo.value;
 const swap = document.getElementById('button');
-const url = "https://v6.exchangerate-api.com/v6/85d74a9472487e7551d8fbc8/latest/"
-function calculateOne(){
-    const currencyOneValue = currencyOne.value;
-    const currencyTwoValue = currencyTwo.value;
-    fetch(`${url}${currencyOneValue}`)
+const url = "https://v6.exchangerate-api.com/v6/85d74a9472487e7551d8fbc8/latest/";
+
+function calculate(data1,data2,amount1,amount2){
+    fetch(`${url}${data1}`)
     .then((res)=> res.json())
     .then((data)=>{
-        const rate = data.conversion_rates[currencyTwoValue];
-        amountTwo.value = (amountOne.value*rate).toFixed(2);
+        const rate = data.conversion_rates[data2];
+        amount2.value = (amount1.value*rate).toFixed(2);
     });
 }
 
-function calculateTwo(){
-    const currencyOneValue = currencyOne.value;
-    const currencyTwoValue = currencyTwo.value;
-    fetch(`${url}${currencyTwoValue}`)
-    .then((res)=> res.json())
-    .then((data)=>{
-        const rate = data.conversion_rates[currencyOneValue];
-        amountOne.value = (amountTwo.value*rate).toFixed(2);
-    });
+function firstInput(){
+    calculate(currencyOneValue,currencyTwoValue,amountOne,amountTwo);
 }
 
+function secondInput(){
+    
+    calculate(currencyTwoValue,currencyOneValue,amountTwo,amountOne);
+}
 
-currencyOne.addEventListener('change', calculateOne);
-amountOne.addEventListener('input', calculateOne);
-currencyTwo.addEventListener('change', calculateTwo);
-amountTwo.addEventListener('input', calculateTwo);
+currencyOne.addEventListener('change', firstInput);
+amountOne.addEventListener('input', firstInput);
+currencyTwo.addEventListener('change', secondInput);
+amountTwo.addEventListener('input', secondInput);
 
 swap.addEventListener('click', ()=> {
     const temp = currencyOne.value;
     currencyOne.value = currencyTwo.value;
     currencyTwo.value = temp;
-    calculateOne();
-  })
+    firstInput();
+})
