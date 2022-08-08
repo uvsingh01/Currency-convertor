d43cd2da20e331f3196bf62793856454ed0e6b47
uvsingh01 <104893892+uvsingh01@users.noreply.github.com>
2022-08-08 16:33:08 +0530

    final

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const currencyOne= document.getElementById('currency-first');
 const currencyTwo= document.getElementById('currency-second');
 const amountOne = document.getElementById('amount-first');
 const amountTwo = document.getElementById('amount-second');
-const currencyOneValue = currencyOne.value;
-const currencyTwoValue = currencyTwo.value;
 const swap = document.getElementById('button');
 const url = "https://v6.exchangerate-api.com/v6/85d74a9472487e7551d8fbc8/latest/";
 
@@ -17,11 +15,14 @@ function calculate(data1,data2,amount1,amount2){
 }
 
 function firstInput(){
+    let currencyOneValue = currencyOne.value;
+    let currencyTwoValue = currencyTwo.value;
     calculate(currencyOneValue,currencyTwoValue,amountOne,amountTwo);
 }
 
 function secondInput(){
-    
+    let currencyOneValue = currencyOne.value;
+    let currencyTwoValue = currencyTwo.value;
     calculate(currencyTwoValue,currencyOneValue,amountTwo,amountOne);
 }
 
